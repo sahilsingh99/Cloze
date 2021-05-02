1fbd1e61826f6bbf7db57d01e8c704973e15f686
sahilsingh99 <sahilkundu.1234gmail.com>
2021-05-02 10:50:06 +0530

    all set

--- backend/data/products.js
+++ backend/data/products.js
@@ -2,10 +2,9 @@ const products = [
   {
     name: 'Girls Frock',
     image: '/images/frock.jpg',
-    description:
-      'Bluetooth technology lets you connect it with compatible devices wirelessly High-quality AAC audio offers immersive listening experience Built-in microphone allows you to take calls while working',
-    brand: 'Apple',
-    category: 'Electronics',
+    category: 'clothes',
+    description: 'Frock',
+    brand: 'local',
     price: 89.99,
     countInStock: 3,
     rating: 0,
@@ -14,10 +13,9 @@ const products = [
   {
     name: 'Bridal Clothes',
     image: '/images/bridalClothes.jpg',
-    description:
-      'Introducing the iPhone 11 Pro. A transformative triple-camera system that adds tons of capability without complexity. An unprecedented leap in battery life',
-    brand: 'Apple',
-    category: 'Electronics',
+    category: 'clothes',
+    description: 'Bridal',
+    brand: 'local',
     price: 599.99,
     countInStock: 10,
     rating: 0,
@@ -26,10 +24,9 @@ const products = [
   {
     name: 'Ladies Gown',
     image: '/images/gown.jpg',
-    description:
-      'Characterized by versatile imaging specs, the Canon EOS 80D further clarifies itself using a pair of robust focusing systems and an intuitive design',
-    brand: 'Cannon',
-    category: 'Electronics',
+    category: 'clothes',
+    description: 'Gown',
+    brand: 'local',
     price: 929.99,
     countInStock: 0,
     rating: 0,
@@ -38,10 +35,9 @@ const products = [
   {
     name: 'Kurta',
     image: '/images/kurta.jpg',
-    description:
-      'The ultimate home entertainment center starts with PlayStation. Whether you are into gaming, HD movies, television, music',
-    brand: 'Sony',
-    category: 'Electronics',
+    category: 'clothes',
+    description: 'Kurta',
+    brand: 'local',
     price: 399.99,
     countInStock: 10,
     rating: 0,
@@ -50,10 +46,9 @@ const products = [
   {
     name: 'Shirt',
     image: '/images/shirt.jpg',
-    description:
-      'Get a better handle on your games with this Logitech LIGHTSYNC gaming mouse. The six programmable buttons allow customization for a smooth playing experience',
-    brand: 'Logitech',
-    category: 'Electronics',
+    category: 'clothes',
+    description: 'Shirt',
+    brand: 'local',
     price: 49.99,
     countInStock: 7,
     rating: 0,
@@ -62,10 +57,9 @@ const products = [
   {
     name: 'Sikh Wearing',
     image: '/images/sikhWearing.jpg',
-    description:
-      'Meet Echo Dot - Our most popular smart speaker with a fabric design. It is our most compact smart speaker that fits perfectly into small space',
-    brand: 'Amazon',
-    category: 'Electronics',
+    category: 'clothes',
+    description: 'Sikh Wearing',
+    brand: 'local',
     price: 29.99,
     countInStock: 0,
     rating: 0,
@@ -74,10 +68,9 @@ const products = [
   {
     name: 'Ladies Suit',
     image: '/images/suit.jpg',
-    description:
-      'Meet Echo Dot - Our most popular smart speaker with a fabric design. It is our most compact smart speaker that fits perfectly into small space',
-    brand: 'Amazon',
-    category: 'Electronics',
+    category: 'clothes',
+    description: 'Suit',
+    brand: 'local',
     price: 29.99,
     countInStock: 0,
     rating: 0,
@@ -86,10 +79,9 @@ const products = [
   {
     name: 'Tshirt for Men',
     image: '/images/tshirt.jpg',
-    description:
-      'Meet Echo Dot - Our most popular smart speaker with a fabric design. It is our most compact smart speaker that fits perfectly into small space',
-    brand: 'Amazon',
-    category: 'Electronics',
+    category: 'clothes',
+    description: 'Tshirt',
+    brand: 'local',
     price: 29.99,
     countInStock: 0,
     rating: 0,
